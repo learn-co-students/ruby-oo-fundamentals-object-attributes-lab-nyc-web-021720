c17a801c956a7a0363c7b69135c9b0a57b3f54f8
zachbrown12 <zachmb22@gmail.com>
2020-02-19 15:42:29 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+   def name=(dname)
+     @name = dname
+   end
+
+   def name
+     @name
+   end
+
+   def breed=(dbreed)
+     @breed = dbreed
+   end
+
+   def breed 
+     @breed
+   end
+end
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+    def name=(pname)
+        @name = pname
+    end
+
+    def name
+        @name
+    end
+ 
+    def job=(pjob)
+        @job = pjob
+    end
+
+    def job
+        @job
+    end
+end
+
+
