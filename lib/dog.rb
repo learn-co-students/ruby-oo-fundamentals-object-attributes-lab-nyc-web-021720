541dc52d8d185936327f7af13524eca0a6d2dd9b
devmullets <devmullets@gmail.com>
2020-02-19 16:41:12 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+    def initialize
+        @name
+        # @breed = breed
+    end 
+
+    def name=(new_name)
+        @name = new_name
+    end
+
+    def name
+        @name
+    end 
+
+    def breed=(breed_name)
+        @breed = breed_name
+    end
+
+    def breed
+        @breed
+    end 
+
+end
+
+# lassie = Dog.new("Kanye")
+# puts lassie.name
